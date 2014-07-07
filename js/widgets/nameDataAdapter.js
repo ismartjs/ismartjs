e657123c60d16e5a9429930f22f6359fa48f09e5
ling <daigua1028@gmail.com>
2014-07-08 00:29:01 +0800

    修改bug

--- js/widgets/nameDataAdapter.js
+++ js/widgets/nameDataAdapter.js
@@ -13,7 +13,7 @@
                     //则把它声明成为一个基本控件
                     nameNode.attr(Smart.defineKey, "");
                 }
-                var attrName = Smart.optionAttrName("data", "key");
+                var attrName = Smart.optionAttrName("smart", "key");
                 if(Smart.isEmpty(nameNode.attr(attrName))){
                     nameNode.attr(attrName, nameNode.attr("name"));
                 }
