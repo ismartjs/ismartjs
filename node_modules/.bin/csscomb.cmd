af326fb8a542b1858f8fb01a9227e5ea43be069f
175309744@qq.com <l19841028>
2016-06-22 11:52:28 +0800

    Merge branch 'master' of https://github.com/CTOSPACE/ismartjs
    Conflicts:
    	node_modules/core-util-is/lib/util.js
    	node_modules/core-util-is/package.json
    	node_modules/iconv-lite/.npmignore
    	node_modules/iconv-lite/.travis.yml
    	node_modules/iconv-lite/README.md
    	node_modules/iconv-lite/package.json
    	node_modules/inherits/package.json
    	node_modules/isarray/README.md
    	node_modules/isarray/index.js
    	node_modules/isarray/package.json
    	node_modules/lodash/README.md
    	node_modules/lodash/package.json
    	node_modules/readable-stream/README.md
    	node_modules/readable-stream/lib/_stream_duplex.js
    	node_modules/readable-stream/lib/_stream_passthrough.js
    	node_modules/readable-stream/lib/_stream_readable.js
    	node_modules/readable-stream/lib/_stream_transform.js
    	node_modules/readable-stream/lib/_stream_writable.js
    	node_modules/readable-stream/package.json
    	node_modules/readable-stream/readable.js
    	node_modules/string_decoder/package.json
    	package.json

--- node_modules/.bin/csscomb.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\csscomb\bin\csscomb" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\csscomb\bin\csscomb" %*
-)
