b1e2369ce03a3e2c20b7f177659670a3bd7468c0
ling.lv <165101987@qq.com>
2018-02-26 17:02:59 +0800

    S.confirm 增加输入确认功能，使得只有在输入正确的情况下才能进行确认操作

--- js/bootstrap-plugins/input-select.js
+++ js/bootstrap-plugins/input-select.js
@@ -3,15 +3,14 @@
     "use strict";
     $(document).delegate("input,textarea", "focus", function () {
         var node = $(this);
-        if (node.attr("data-focus-select") == 'false') {
-            return;
+        if (node.attr("data-auto-select")) {
+            setTimeout(function () {
+                if(document.activeElement != node[0]){
+                    return;
+                }
+                node.select();
+            }, 100);
         }
-        setTimeout(function () {
-            if(document.activeElement != node[0]){
-                return;
-            }
-            node.select();
-        }, 100);
     })
 
 }(jQuery)
